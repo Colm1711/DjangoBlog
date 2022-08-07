f9ad29f235e4c94e33447e185b7be13de23a2bd3
Colm1711 <cosullivan1711@gmail.com>
2022-08-07 19:38:17 +0000

    add sites, pagination and view likes + post list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+# create view code
+class PostList(generic.ListView):
+    # setting model to post
+    model = Post
+    # set this to 1 for posted comments and order in descending by created on
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
